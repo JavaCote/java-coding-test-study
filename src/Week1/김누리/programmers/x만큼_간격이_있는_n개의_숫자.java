d9281b5bdf91b7721b0a711d3ff87bec5e6190ea
NRKim <ning9303@gmail.com>
2025-04-27 15:08:30 +0900

    06.  x만큰 간격이 있는 n개의 숫자 : Stream API 이용한 버전

---
+++ src/Week1/김누리/programmers/x만큼_간격이_있는_n개의_숫자.java
@@ -0,0 +1,35 @@
+package Week1.김누리.programmers;
+
+import java.util.stream.IntStream;
+
+/**
+ * PackageName : Week1.김누리.programmers
+ * FileName    : x만큼_간격이_있는_n개의_숫자
+ * Author      : 김누리
+ * Date        : 2025-04-25
+ * Description :
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 2025. 04. 25.     김누리               Initial creation
+ * 2025. 04. 27.     김누리               Stream API를 이용하지 않은 버전 작성
+ * 2025. 04. 27.     김누리               Stream API를 이용하는 버전 작성
+ */
+public class x만큼_간격이_있는_n개의_숫자 {
+	public long[] solution(int x, int n) {
+		//  Stream API 이용 버전
+		return IntStream.rangeClosed(1,n)
+			.mapToLong(i -> (long)x*(i))
+			.toArray();
+
+		//  Stream API 이용하지 않는 버전
+	}
+
+	public void call_solution(int x, int n) {
+		long[] answer = solution(x, n);
+
+		for(long i : answer) {
+			System.out.println(i);
+		}
+	}
+}
