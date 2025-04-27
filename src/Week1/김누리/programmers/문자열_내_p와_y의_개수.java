3c972e56c803bc8422d32a15cbebc319dcdf504e
NRKim <ning9303@gmail.com>
2025-04-27 16:51:37 +0900

    10.  문자열 내 p와 y의 개수

--- src/Week1/김누리/programmers/문자열_내_p와_y의_개수.java
+++ src/Week1/김누리/programmers/문자열_내_p와_y의_개수.java
@@ -12,4 +12,25 @@ package Week1.김누리.programmers;
  * 2025. 04. 25.     김누리               Initial creation
  */
 public class 문자열_내_p와_y의_개수 {
+	public boolean solution(String s) {
+		boolean answer = true;
+		int pCnt = 0;
+		int yCnt = 0;
+		String[] tmp = s.split("");
+
+		for(String s2 : tmp) {
+			if(s2.equalsIgnoreCase("p")) pCnt++;
+			if(s2.equalsIgnoreCase("y")) yCnt++;
+		}
+
+		if(pCnt != yCnt) answer = false;
+
+		return answer;
+	}
+
+	public void call_solution(String s) {
+		boolean answer = solution(s);
+
+		System.out.println(answer);
+	}
 }
