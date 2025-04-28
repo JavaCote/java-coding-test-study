7d63382adddfbf836069cea02be3a8b899430cff
NRKim <ning9303@gmail.com>
2025-04-28 14:56:16 +0900

    하샤드 수 풀이 완료

--- src/Week1/김누리/programmers/하샤드_수.java
+++ src/Week1/김누리/programmers/하샤드_수.java
@@ -12,4 +12,23 @@ package Week1.김누리.programmers;
  * 2025. 04. 25.     김누리               Initial creation
  */
 public class 하샤드_수 {
+	public boolean solution(int x) {
+		boolean answer = true;
+		int harshad = 0;
+		int tmp = x;
+
+		while (tmp > 0) {
+			harshad += tmp % 10; // 자리수 더하기
+			tmp /= 10;
+		}
+
+		answer = x%harshad == 0 ? true : false;
+
+		return answer;
+	}
+
+	public void call_solution(int x) {
+		boolean answer = solution(x);
+		System.out.println(answer);
+	}
 }
