fcb3dca0721dea3b514f2bb566b681d8781196dc
leesangeok <wyzmqkr11@naver.com>
2025-05-08 17:26:17 +0900

    ì최소 직사각형

---
+++ week2/이상억/backjoon/큐_2.java
@@ -0,0 +1,49 @@
+package week2.이상억.progarmmers;
+
+/**
+ * PackageName : week2.이상억.programmers
+ * FileName    : 큐_2
+ * Author      : sangeok
+ * Date        : 2025. 5. 7.
+ * Description :
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 2025. 5. 7.     sangeok               Initial creation
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        LinkedList<Integer> queue = new LinkedList<>();
+        int last = -1;
+        for (int i = 0; i < n; i++) {
+            String cmd = br.readLine();
+
+            if (cmd.startsWith("push")) {
+                int x = Integer.parseInt(cmd.split(" ")[1]);
+                queue.addLast(x);
+                last = x;
+            } else if (cmd.equals("pop")) {
+                sb.append(queue.isEmpty() ? -1 : queue.removeFirst()).append('\n');
+            } else if (cmd.equals("size")) {
+                sb.append(queue.size()).append('\n');
+            } else if (cmd.equals("empty")) {
+                sb.append(queue.isEmpty() ? 1 : 0).append('\n');
+            } else if (cmd.equals("front")) {
+                sb.append(queue.isEmpty() ? -1 : queue.getFirst()).append('\n');
+            } else if (cmd.equals("back")) {
+                sb.append(queue.isEmpty() ? -1 : last).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
+}
