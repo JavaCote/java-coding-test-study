846bcc53f1aeaf4e9bcb2e7de70ba8a2d308c8d4
hellonaeunkim <hellonaeunkim@gmail.com>
2025-05-28 20:18:45 +0900

    클래스 명 변경 및 패키지 구조 리팩토링, 주석 설명 추가

--- week5/김나은/programmers/구명보트.java
+++ week5/김나은/programmers/구명보트.java
@@ -1,21 +1,29 @@
 package week5.김나은.programmers;
 
-import java.util.*;
+import java.util.Arrays;
 
-class Solution {
-    public int solution(int[] people, int limit) {
-        int answer = 0;
+public class 구명보트 {
 
-        Arrays.sort(people);
-        int min = 0;
+    class Solution {
 
-        for(int max = people.length - 1; min <= max; max--) {
-            if(people[min] + people[max] <= limit) {
-                min++;
+        public int solution(int[] people, int limit) {
+            int answer = 0;
+
+            // 사람들의 몸무게를 오름차순으로 정렬
+            Arrays.sort(people);
+            // 가장 가벼운 사람의 인덱스
+            int min = 0;
+
+            // max는 가장 무거운 사람의 인덱스부터 시작해서 점점 앞으로 이동
+            for (int max = people.length - 1; min <= max; max--) {
+                // 가장 가벼운 사람(min)과 가장 무거운 사람(max)을 같이 태울 수 있는 경우
+                if (people[min] + people[max] <= limit) {
+                    min++; // 가벼운 사람도 태웠으니 다음으로 이동
+                }
+                answer++; // 무거운 사람은 무조건 태우기 때문에 보트 1대 추가
             }
-            answer++;
-        }
 
-        return answer;
+            return answer; // 총 필요한 보트 수 반환
+        }
     }
 }
