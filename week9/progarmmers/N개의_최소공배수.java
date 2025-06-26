0d72c79784d516774f0e7608cba7abe281bd65bc
leesangeok <wyzmqkr11@naver.com>
2025-06-26 17:39:07 +0900

    최소 공배수

--- week9/progarmmers/N개의_최소공배수.java
+++ week9/progarmmers/N개의_최소공배수.java
@@ -60,3 +60,29 @@ class Solution {
         return fxmap;
     }
 }
+
+
+// import java.util.*;
+//
+//class Solution {
+//    public int solution(int[] arr) {
+//        int lcm = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            lcm = lcm(lcm, arr[i]);
+//        }
+//        return lcm;
+//    }
+//
+//    private int lcm(int a, int b) {
+//        return a * b / gcd(a, b);
+//    }
+//
+//    private int gcd(int a, int b) {
+//        while (b != 0) {
+//            int tmp = a % b;
+//            a = b;
+//            b = tmp;
+//        }
+//        return a;
+//    }
+//}
