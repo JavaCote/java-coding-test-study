0882c3fda9826d036796a8b0a8a275bf8c26f66f
leesangeok <wyzmqkr11@naver.com>
2025-08-06 21:00:37 +0900

    n^2 배열 자르기

---
+++ week15/이상억/progarmmers/시소_짝꿍.java
@@ -0,0 +1,14 @@
+// 중심으로부터 2,3,4 거리 지점에 좌석이 하나씩
+
+// 두 명이 마주보고 탄다 했을 때 토크 크기가 상쇄 되어 균형을 이룬다면 ? -> 시소 짝궁
+// 탑승한 사람의 무게, 시소 축과 좌석 간의 거리의 곱이 양 쪽다 같다면 시소 짝궁.
+
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+
+
+        return answer;
+    }
+}
