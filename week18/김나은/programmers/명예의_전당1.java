d9b5b408eb3efcc9cfcd18cff87d5dc5b0ceaa18
hellonaeunkim <hellonaeunkim@gmail.com>
2025-08-29 21:08:39 +0900

    Lv.1 명예의 전당 (1)

--- week18/김나은/programmers/명예의_전당1.java
+++ week18/김나은/programmers/명예의_전당1.java
@@ -1,14 +1,31 @@
 package week18.김나은.programmers;
 
-/**
- * PackageName : week18.김나은.programmers
- * FileName    : 명예의_전당1
- * Author      : hellonaeunkim
- * Date        : 8/27/25
- * Description : 
- * =====================================================================================================================
- * DATE          AUTHOR               NOTE
- * ---------------------------------------------------------------------------------------------------------------------
- * 8/27/25    hellonaeunkim       Initial creation
- */public class 명예의_전당1 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 명예의_전당1 {
+
+    class Solution {
+
+        public int[] solution(int k, int[] score) {
+            int[] answer = new int[score.length];
+
+            // 명예의 전당: 지금까지 점수 중 상위 k개만 관리하는 리스트
+            List<Integer> rank = new ArrayList<>();
+
+            for (int i = 0; i < score.length; i++) {
+                // k번째 이전 인덱스까지 rank에 저장
+                rank.add(score[i]);
+
+                // rank 리스트의 크기가 k보다 크면 가장 낮은 점수 제거
+                if (rank.size() > k) {
+                    rank.remove(Collections.min(rank));
+                }
+                // 현재 명예의 전당에서 가장 낮은 점수를 발표
+                answer[i] = Collections.min(rank);
+            }
+            return answer;
+        }
+    }
 }
