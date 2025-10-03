3c621a8dcfebfcafe8efa5b09dff722cc9c92f9e
KangBaekGwa <ksu9801@gmail.com>
2025-10-03 16:04:40 +0900

    누락 수정

--- week23/강성욱/baekjoon/벌집.java
+++ week23/강성욱/baekjoon/벌집.java
@@ -1,6 +1,7 @@
 package week23.강성욱.baekjoon;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * PackageName : week23.강성욱.baekjoon
@@ -14,8 +15,23 @@ import java.io.IOException;
  * 2025-09-29     Baekgwa               Initial creation
  */
 public class 벌집 {
+	// 입력값에 따라서 범위가 지정됨
+	// 1 -> 2 ~ 7 -> 8 ~ 19 ...
+	// 1 -> 6개 -> 12개 -> 18개 ... 6개씩 늘어남
 	public class Main {
-		public static void main(String[] args) throws IOException {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			long N = sc.nextLong();
+			sc.close();
+
+			long cnt = 1;
+			long now = 1;
+			while(now < N) {
+				now += cnt * 6;
+				cnt++;
+			}
+
+			System.out.println(cnt);
 		}
 	}
 }
