ba16e6358823375522d575aad666fbe148b042c5
KangBaekGwa <ksu9801@gmail.com>
2025-10-03 15:27:52 +0900

    23주차 완료 - 한문제 못품

---
+++ week23/강성욱/baekjoon/벌집.java
@@ -0,0 +1,21 @@
+package week23.강성욱.baekjoon;
+
+import java.io.IOException;
+
+/**
+ * PackageName : week23.강성욱.baekjoon
+ * FileName    : 벌집
+ * Author      : Baekgwa
+ * Date        : 2025-09-29
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 2025-09-29     Baekgwa               Initial creation
+ */
+public class 벌집 {
+	public class Main {
+		public static void main(String[] args) throws IOException {
+		}
+	}
+}
