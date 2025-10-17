b7ea4b136ec16cab9070c24a190844464d02b4f5
hellonaeunkim <hellonaeunkim@gmail.com>
2025-10-17 09:53:04 +0900

    Silver IV. 스택

--- week24/김나은/baekjoon/스택.java
+++ week24/김나은/baekjoon/스택.java
@@ -22,22 +22,31 @@ public class 스택 {
             Deque<Integer> stack = new ArrayDeque<>();
 
             int i = 0;
+            // 주어진 명령 개수(capacity)만큼 반복 수행
             while (i < capacity) {
+                // 한 줄을 공백 기준으로 나누기
                 st = new StringTokenizer(br.readLine());
                 switch (st.nextToken()) {
                     case "push":
+                        // 정수 X를 스택 맨 위에 추가
                         stack.push(Integer.parseInt(st.nextToken()));
                         break;
                     case "pop":
+                        // 스택 맨 위에 있는 정수를 빼고, 그 수를 출력
+                        // 만약 스택에 들어있는 정수가 없는 경우 -1 출력
                         System.out.println(stack.isEmpty() ? -1 : stack.pop());
                         break;
                     case "size":
+                        // 스택에 들어있는 정수의 개수 출력
                         System.out.println(stack.size());
                         break;
                     case "empty":
+                        // 스택이 비어있으면 1, 아니면 0 출력
                         System.out.println(stack.isEmpty() ? 1 : 0);
                         break;
                     case "top":
+                        // 스택 맨 위에 있는 정수를 출력
+                        // 만약 스택에 들어있는 정수가 없는 경우 -1 출력
                         System.out.println(stack.isEmpty() ? -1 : stack.peek());
                         break;
                 }
