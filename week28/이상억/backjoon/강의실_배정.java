75cb20eafb8096c8a8c249c0c6758c4e01ff596c
leesangeok <wyzmqkr11@naver.com>
2025-11-12 15:24:46 +0900

    week28

--- week28/이상억/backjoon/강의실_배정.java
+++ week28/이상억/backjoon/강의실_배정.java
@@ -1,22 +1,40 @@
+import java.io.*;
 import java.util.*;
 
-class Solution {
-    public int solution(int[] A, int[] B) {
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        Arrays.sort(A);
-        Arrays.sort(B);
+        int[][] lectures = new int[N][2];
 
-        int i = 0;
-        int j = 0;
-        int win = 0;
-
-        while( i < A.length && j < B.length) {
-            if(B[j] > A[i]){
-                win ++;
-                i++;
-            }
-            j++;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            lectures[i][0] = Integer.parseInt(st.nextToken()); // 시작 시간
+            lectures[i][1] = Integer.parseInt(st.nextToken()); // 종료 시간
         }
-        return win;
+
+        List<int[]> events = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            events.add(new int[]{lectures[i][0], 1});   // 시작: +1
+            events.add(new int[]{lectures[i][1], -1});  // 종료: -1
+        }
+
+
+        events.sort((a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
+
+        int now = 0;
+        int maxRoom = 0;
+
+        for (int[] e : events) {
+            now += e[1];
+            maxRoom = Math.max(maxRoom, now);
+        }
+
+        System.out.println(maxRoom);
     }
 }
