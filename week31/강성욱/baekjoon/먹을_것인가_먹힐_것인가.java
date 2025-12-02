9df4a9600ee27e966f8e2fdd9e4e145fbc082ec9
KangBaekGwa <ksu9801@gmail.com>
2025-12-02 14:15:55 +0900

    feat: 31주차 완료

---
+++ week31/강성욱/baekjoon/먹을_것인가_먹힐_것인가.java
@@ -0,0 +1,72 @@
+package week31.강성욱.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * PackageName : week31.강성욱.baekjoon
+ * FileName    : 먹을_것인가_먹힐_것인가
+ * Author      : Baekgwa
+ * Date        : 25. 12. 2.
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 25. 12. 2.     Baekgwa               Initial creation
+ */
+public class 먹을_것인가_먹힐_것인가 {
+	public class Main {
+
+		public static void main(String[] args) throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int testCase = Integer.parseInt(br.readLine());
+			StringBuilder sb = new StringBuilder();
+
+			for(int i=0; i<testCase; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int aCount = Integer.parseInt(st.nextToken());
+				int bCount = Integer.parseInt(st.nextToken());
+
+				int[] a = new int[aCount];
+				int[] b = new int[bCount];
+
+				st = new StringTokenizer(br.readLine());
+				for(int idx=0; idx<aCount; idx++) {
+					a[idx] = Integer.parseInt(st.nextToken());
+				}
+
+				st = new StringTokenizer(br.readLine());
+				for(int idx=0; idx<bCount; idx++) {
+					b[idx] = Integer.parseInt(st.nextToken());
+				}
+
+				// a 를 iter 돌면서 확인
+				Arrays.sort(b); //b 배열 한번 정리해줘야함 정렬
+				int result = 0;
+				for (int nowA : a) {
+					int idx = Arrays.binarySearch(b, nowA);
+
+					// 음수면, 그곳에 넣으면 되는 index 값 반환됨.
+					int count = 0;
+					if(idx < 0) {
+						count = idx * -1 - 1;
+					} else {
+						while(idx > 0 && b[idx -1] == nowA) {
+							idx--;
+						}
+						count = idx;
+					}
+
+					result += count;
+				}
+
+				sb.append(result).append("\n");
+			}
+
+			System.out.println(sb);
+		}
+	}
+}
---
+++ week31/강성욱/programmers/비밀_코드_해독.java
@@ -0,0 +1,81 @@
+package week31.강성욱.programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * PackageName : week31.강성욱.programmers
+ * FileName    : 비밀_코드_해독
+ * Author      : Baekgwa
+ * Date        : 25. 12. 2.
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 25. 12. 2.     Baekgwa               Initial creation
+ */
+public class 비밀_코드_해독 {
+	// 조합 다 만들어서 처리
+	// 30c5
+	// 30 * 29 * 28 * 27 * 26 / 5!
+	class Solution {
+		public static List<List<Integer>> combList = new ArrayList<>();
+
+		public int solution(int n, int[][] q, int[] ans) {
+			//백트래킹을 활용한 조합 만들기
+			makeComb(1, n, new ArrayList<>());
+
+			int result = validComb(q, ans);
+
+			return result;
+		}
+
+		// 생성된 조합들을 조회하며 검증 확인
+		private int validComb(int[][] q, int[] ans) {
+			int count = 0;
+
+			// contains로 검증하면 될듯?
+			for(List<Integer> comb : combList) {
+
+				boolean isValid = true;
+
+				for(int idx=0; idx<ans.length; idx++) {
+					int correctCount = ans[idx];
+
+					int nowCorrectCount = 0;
+					for(int i=0; i<q[idx].length; i++) {
+						if(comb.contains(q[idx][i])) nowCorrectCount++;
+					}
+
+					if(correctCount != nowCorrectCount) {
+						isValid = false;
+						break;
+					}
+				}
+
+				if(isValid) count++;
+			}
+
+			return count;
+		}
+
+		private void makeComb(int start, int n, List<Integer> nowComb) {
+			// 5개 다 채우면, 끝
+			if(nowComb.size() >= 5) {
+				combList.add(new ArrayList<>(nowComb));
+				return;
+			}
+
+			// 1
+			// -> 1, 2
+			// -> 1, 2, 3
+			// -> 1, 2, 3, 4...
+			// -> 1, 3
+			for(int i=start; i<=n; i++) {
+				nowComb.add(i);
+				makeComb(i+1, n, nowComb);
+				nowComb.remove(nowComb.size() -1); //백트래킹, 마지막꺼 지우기
+			}
+		}
+	}
+}
